7884efa0dd97a7e46ac7b492a887c897049ecc05
fanistheolog11 <84383625+fanistheolog11@users.noreply.github.com>
2021-05-18 16:36:08 +0300

    Update OnItemClicked.java

--- mindmappingandroidlibrary/src/main/java/me/jagar/mindmappingandroidlibrary/Listeners/OnItemClicked.java
+++ mindmappingandroidlibrary/src/main/java/me/jagar/mindmappingandroidlibrary/Listeners/OnItemClicked.java
@@ -4,5 +4,4 @@ import me.jagar.mindmappingandroidlibrary.Views.Item;
 
 public interface OnItemClicked {
     void OnClick(Item item);
-    Toast.makeText(MainActivity.this, "Title" + item, Toast.LENGTH_SHORT).show();
 }
